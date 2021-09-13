d748a3f5229b5a5a03dfe9be85d7c1ca438865cf
Christopher Horn <christopher.horn@travelport.com>
2021-09-13 21:55:10 +0300

    server: create user controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,52 @@
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
+exports.register = async (req, res) => {
+  console.log(req);
+
+  const { name, email, password } = req.body;
+
+  const userAlreadyRegistered = await User.findOne({ where: { email } }).catch(
+    (err) => {
+      console.error("Error: ", err);
+    }
+  );
+
+  if (userAlreadyRegistered)
+    res
+      .status(409)
+      .send({ message: "User already registered with this email" });
+
+  const newUser = new User({ name, email, password });
+
+  const savedUser = await newUser.save().catch((err) => {
+    console.error("Error: ", err);
+    res.status(500).send({ message: "Cannot register user with this email" });
+  });
+
+  if (savedUser)
+    res.status(200).send({ message: "User registered successfully" });
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!password || !email) {
+    res.status(400).send({ message: "Both email and password are required!" });
+  }
+
+  const user = await User.findOne({ where: { email } });
+
+  if (!user || user.password !== password)
+    res
+      .status(400)
+      .send({ message: "Provided email and/or password do not match!" });
+
+  const jwToken = jwt.sign(
+    { id: user.id, email: user.email },
+    "CP_MAIL_SECRET"
+  );
+
+  res.status(200).send({ message: "Welcome back!", token: jwToken });
+};
