b4104c5d167eb7249955d5a7339174243a592f05
Christopher Horn <christopher.horn@travelport.com>
2021-09-25 20:03:07 +0300

    server: use cors and test authentication middleware

--- server/routes/initRoutes.js
+++ server/routes/initRoutes.js
@@ -1,11 +1,16 @@
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const userController = require("../controllers/userController");
+const mailController = require("../controllers/mailController");
+const verifyToken = require("../middlewares/authenticate");
 
 module.exports = function initRoutes(app) {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
+  app.use(cors());
   app.post("/register", userController.register);
   app.post("/login", userController.login);
+  app.get("/tokentest", [verifyToken], mailController.get);
 
   app.get("/", (req, res) => {
     res.status(200).send({ message: "Hello there!" });
