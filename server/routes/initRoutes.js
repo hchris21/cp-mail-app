063b3abbdbe59114ac34b27842a6f2c25418d4c7
Christopher Horn <christopher.horn@travelport.com>
2021-10-02 16:02:20 +0300

    server: use cookie-parser and format file

--- server/routes/initRoutes.js
+++ server/routes/initRoutes.js
@@ -1,16 +1,28 @@
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const userController = require("../controllers/userController");
 const mailController = require("../controllers/mailController");
 const verifyToken = require("../middlewares/authenticate");
 
 module.exports = function initRoutes(app) {
+  app.use(cookieParser());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(cors());
+
+  // User
   app.post("/register", userController.register);
   app.post("/login", userController.login);
-  app.get("/tokentest", [verifyToken], mailController.get);
+
+  // Mail
+  app.post("/sendmail", [verifyToken], mailController.sendMail);
+  app.get("/outbox", [verifyToken], mailController.getSentMails);
+  app.get("/inbox", [verifyToken], mailController.getReceivedMails);
+
+  // Reply
+  app.post("/reply", [verifyToken], mailController.sendReply);
+  app.get("/replies", [verifyToken], mailController.getRepliesByMailId);
 
   app.get("/", (req, res) => {
     res.status(200).send({ message: "Hello there!" });
