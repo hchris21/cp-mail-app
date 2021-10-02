502905dd45929b3503644301f5e787695db644f6
Christopher Horn <christopher.horn@travelport.com>
2021-10-02 16:01:40 +0300

    server: adapt to db reconfiguration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,11 @@
 const jwt = require("jsonwebtoken");
 
-const User = require("../models/User");
 const config = require("../config/config");
+const db = require("../utils/database");
+const User = db.users;
 
 exports.register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { first_name, last_name, email, password } = req.body;
 
   const userAlreadyRegistered = await User.findOne({ where: { email } }).catch(
     (err) => {
@@ -12,40 +13,48 @@ exports.register = async (req, res) => {
     }
   );
 
-  if (userAlreadyRegistered)
-    res
+  if (userAlreadyRegistered) {
+    return res
       .status(409)
       .send({ message: "User already registered with this email" });
+  }
 
-  const newUser = new User({ name, email, password });
+  const newUser = new User({ first_name, last_name, email, password });
 
   const savedUser = await newUser.save().catch((err) => {
     console.error("Error: ", err);
-    res.status(500).send({ message: "Cannot register user with this email" });
+    return res
+      .status(500)
+      .send({ message: "Cannot register user with this email" });
   });
 
   if (savedUser)
-    res.status(200).send({ message: "User registered successfully" });
+    return res.status(200).send({ message: "User registered successfully" });
 };
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
 
   if (!password || !email) {
-    res.status(400).send({ message: "Both email and password are required!" });
+    return res
+      .status(400)
+      .send({ message: "Both email and password are required!" });
   }
 
   const user = await User.findOne({ where: { email } });
 
   if (!user || user.password !== password)
-    res
+    return res
       .status(400)
       .send({ message: "Provided email and/or password do not match!" });
 
-  const jwToken = jwt.sign(
+  const jwtToken = jwt.sign(
     { id: user.id, email: user.email },
     config.jwtSecret
   );
 
-  res.status(200).send({ message: "Welcome back!", token: jwToken });
+  return res
+    .cookie("jwt_token", jwtToken)
+    .status(200)
+    .send({ message: "Welcome back!" });
 };
