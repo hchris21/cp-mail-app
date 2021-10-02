b5da264602ca722298d76410871f5ccd98d1ed5b
Christopher Horn <christopher.horn@travelport.com>
2021-10-02 16:02:37 +0300

    server: use cookies in authentication middleware

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -2,21 +2,22 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/config");
 
 module.exports = verifyJwt = (req, res, next) => {
-  let token = req.headers["x-access-token"];
+  const { cookies } = req;
 
-  if (!token) {
+  if (!cookies.jwt_token) {
     return res.status(403).send({
       message: "To access this page you need an authentication token!",
     });
   }
 
-  jwt.verify(token, config.jwtSecret, (error, decoded) => {
+  jwt.verify(cookies.jwt_token, config.jwtSecret, (error, decoded) => {
     if (error) {
       return res
         .status(401)
         .send({ message: "You are not authorized to access this page!" });
     }
     req.userId = decoded.id;
+    req.email = decoded.email;
     next();
   });
 };
