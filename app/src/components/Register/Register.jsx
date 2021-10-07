0a8635a33c442ad2c977ce60c6da351cae37d794
Christopher Horn <christopher.horn@travelport.com>
2021-10-07 21:04:54 +0300

    app: enhance Register component

--- app/src/components/Register/Register.jsx
+++ app/src/components/Register/Register.jsx
@@ -1,22 +1,51 @@
 import React, { useState } from "react";
-import { Button, Container, CssBaseline, TextField } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import {
+  Button,
+  Box,
+  Container,
+  CssBaseline,
+  TextField,
+} from "@material-ui/core";
 import useRegisterStyles from "./makeRegisterStyles";
+import fetchRegister from "../../core/fetchRegister";
 
 const Login = () => {
   const classes = useRegisterStyles();
+  const history = useHistory();
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  };
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
-  const handleOnSubmit = (e) => {
+  const handleOnSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    const response = await fetchRegister({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+
+    if (response.status === 200) {
+      history.push("/");
+    } else {
+      setError("User already registered!");
+    }
   };
 
   return (
@@ -24,6 +53,38 @@ const Login = () => {
       <CssBaseline />
       <div className={classes.paper}>
         <form className={classes.form} onSubmit={handleOnSubmit} noValidate>
+          <TextField
+            autoComplete="first-name"
+            id="first-name"
+            label="First name"
+            margin="normal"
+            name="first-name"
+            onChange={handleFirstNameChange}
+            value={firstName}
+            variant="outlined"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            autoFocus
+            fullWidth
+            required
+          />
+          <TextField
+            autoComplete="last-name"
+            id="last-name"
+            label="Last name"
+            margin="normal"
+            name="last-name"
+            onChange={handleLastNameChange}
+            value={lastName}
+            variant="outlined"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            autoFocus
+            fullWidth
+            required
+          />
           <TextField
             autoComplete="email"
             id="email"
@@ -56,6 +117,19 @@ const Login = () => {
             required
             fullWidth
           />
+          {error && (
+            <Box
+              sx={{
+                color: "red",
+                fontWeight: "bold",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              {error}
+            </Box>
+          )}
           <Button
             type="submit"
             fullWidth
