680c477a39563c4575144f117016f53bb285d997
Christopher Horn <christopher.horn@travelport.com>
2021-10-07 21:04:41 +0300

    app: extract inline styles

--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-import { styled, alpha } from "@material-ui/core/styles";
+import React from "react";
 import {
   AppBar,
   Box,
@@ -15,50 +14,13 @@ import {
   Search as SearchIcon,
   SendSharp as SendIcon,
 } from "@material-ui/icons";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 5),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
+import useNavbarStyles from "./makeNavbarStyles";
+import { useHistory } from "react-router-dom";
 
 const Navbar = () => {
+  const classes = useNavbarStyles();
+  const history = useHistory();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -71,50 +33,58 @@ const Navbar = () => {
           >
             CP MAIL
           </Typography>
-          <Search>
-            <SearchIconWrapper>
+          <div className={classes.search}>
+            <div className={classes.searchIconWrapper}>
               <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
+            </div>
+            <InputBase
               placeholder="Search…"
+              className={classes.searchInputBase}
               inputProps={{ "aria-label": "search" }}
             />
-          </Search>
+          </div>
           <Box sx={{ flexGrow: 1 }} />
           <Box>
             <IconButton
-              size="large"
+              size="medium"
               edge="start"
               color="inherit"
               aria-label="open drawer"
               sx={{ mr: 2 }}
+              onClick={() => history.push("/inbox")}
             >
               <MailIcon />
             </IconButton>
             <IconButton
-              size="large"
+              size="medium"
               edge="start"
               color="inherit"
               aria-label="open drawer"
               sx={{ mr: 2 }}
+              onClick={() => history.push("/sent")}
             >
               <SendIcon />
             </IconButton>
             <IconButton
-              size="large"
+              size="medium"
               edge="start"
               color="inherit"
               aria-label="open drawer"
               sx={{ mr: 2 }}
+              onClick={() => history.push("/mail")}
             >
               <EditIcon />
             </IconButton>
             <IconButton
-              size="large"
+              size="medium"
               edge="start"
               color="inherit"
               aria-label="open drawer"
               sx={{ mr: 2 }}
+              onClick={() => {
+                document.cookie = "";
+                history.push("/");
+              }}
             >
               <ExitIcon />
             </IconButton>
