bf2c144e137d3e6225d042346d0a558067a723be
Christopher Horn <christopher.horn@travelport.com>
2021-10-23 20:07:32 +0300

    server: separate app initialization code

---
+++ server/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+require("./init/initExpress")(app);
+require("./init/initRoutes")(app);
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,8 @@
-const express = require("express");
-const db = require("./utils/database");
-const config = require("./config/config");
-const app = express();
+const config = require("./env");
+const app = require("./app");
 
 app.listen(config.PORT, () => {
   console.log(`App listening at http://localhost:${config.PORT}`);
 });
-
-require("./init/initExpress")(app);
-require("./init/initRoutes")(app);
 
 module.exports = app;
